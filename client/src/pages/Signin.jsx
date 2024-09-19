89b94c8e5e64ca6fc4b9b86a97cefde3fdad5cf5
chiragnayak110393 <94506509+chiragnayak110393@users.noreply.github.com>
2024-09-19 16:12:52 +0530

    Add Redux Toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,14 @@
 
-import { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData(
       {
@@ -17,7 +19,7 @@ export default function SignIn() {
     const handleSubmit = async(e) => {
       e.preventDefault();
       try {
-        setLoading(true);
+        dispatch(signInStart());
       const res = await fetch('api/auth/signin',
         {
           method: 'POST',
@@ -27,22 +29,20 @@ export default function SignIn() {
         body: JSON.stringify(formData),
         });
         const data = await res.json();
+        console.log(data);
         if (data.success === false) {
-          setLoading(false);
-          setError(data.message);
+          dispatch(signInFailure(data.message));
           return;
         }
-        setLoading(false);
-        setError(null);
+        dispatch(signInSuccess(data));
         navigate('/');
       }
       catch (error){
-        setLoading(false);
-        setError(error.message);
+        dispatch(signInFailure(error.message));
       }
        
     };
-    console.log(formData);
+    
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign IN</h1>
