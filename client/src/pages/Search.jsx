97b9c187481791c9b11ca59548784bf7862ab055
chiragnayak110393 <94506509+chiragnayak110393@users.noreply.github.com>
2024-09-28 14:38:10 +0530

    Add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
 
-
 export default function Search() {
   const navigate = useNavigate();
   const [sidebardata, setSidebardata] = useState({
@@ -14,9 +13,11 @@ export default function Search() {
     sort: 'created_at',
     order: 'desc',
   });
+
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [showMore, setShowMore] = useState(false);
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
@@ -26,6 +27,7 @@ export default function Search() {
     const offerFromUrl = urlParams.get('offer');
     const sortFromUrl = urlParams.get('sort');
     const orderFromUrl = urlParams.get('order');
+
     if (
       searchTermFromUrl ||
       typeFromUrl ||
@@ -45,16 +47,25 @@ export default function Search() {
         order: orderFromUrl || 'desc',
       });
     }
+
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
+
     fetchListings();
   }, [location.search]);
+
   const handleChange = (e) => {
     if (
       e.target.id === 'all' ||
@@ -63,9 +74,11 @@ export default function Search() {
     ) {
       setSidebardata({ ...sidebardata, type: e.target.id });
     }
+
     if (e.target.id === 'searchTerm') {
       setSidebardata({ ...sidebardata, searchTerm: e.target.value });
     }
+
     if (
       e.target.id === 'parking' ||
       e.target.id === 'furnished' ||
@@ -77,12 +90,16 @@ export default function Search() {
           e.target.checked || e.target.checked === 'true' ? true : false,
       });
     }
+
     if (e.target.id === 'sort_order') {
       const sort = e.target.value.split('_')[0] || 'created_at';
+
       const order = e.target.value.split('_')[1] || 'desc';
+
       setSidebardata({ ...sidebardata, sort, order });
     }
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams();
@@ -96,27 +113,41 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
   return (
     <div className='flex flex-col md:flex-row'>
       <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
         <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
           <div className='flex items-center gap-2'>
-          <label className='whitespace-nowrap font-semibold'>
+            <label className='whitespace-nowrap font-semibold'>
               Search Term:
             </label>
             <input
               type='text'
               id='searchTerm'
               placeholder='Search...'
-              className='border rounded-lg p-3 w-full
+              className='border rounded-lg p-3 w-full'
               value={sidebardata.searchTerm}
-              onChange={handleChange}'
+              onChange={handleChange}
             />
           </div>
           <div className='flex gap-2 flex-wrap items-center'>
             <label className='font-semibold'>Type:</label>
             <div className='flex gap-2'>
-            <input
+              <input
                 type='checkbox'
                 id='all'
                 className='w-5'
@@ -126,7 +157,7 @@ export default function Search() {
               <span>Rent & Sale</span>
             </div>
             <div className='flex gap-2'>
-            <input
+              <input
                 type='checkbox'
                 id='rent'
                 className='w-5'
@@ -136,7 +167,7 @@ export default function Search() {
               <span>Rent</span>
             </div>
             <div className='flex gap-2'>
-            <input
+              <input
                 type='checkbox'
                 id='sale'
                 className='w-5'
@@ -146,7 +177,7 @@ export default function Search() {
               <span>Sale</span>
             </div>
             <div className='flex gap-2'>
-            <input
+              <input
                 type='checkbox'
                 id='offer'
                 className='w-5'
@@ -159,7 +190,7 @@ export default function Search() {
           <div className='flex gap-2 flex-wrap items-center'>
             <label className='font-semibold'>Amenities:</label>
             <div className='flex gap-2'>
-            <input
+              <input
                 type='checkbox'
                 id='parking'
                 className='w-5'
@@ -169,7 +200,7 @@ export default function Search() {
               <span>Parking</span>
             </div>
             <div className='flex gap-2'>
-            <input
+              <input
                 type='checkbox'
                 id='furnished'
                 className='w-5'
@@ -199,21 +230,33 @@ export default function Search() {
         </form>
       </div>
       <div className='flex-1'>
-      <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
+        <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
           Listing results:
         </h1>
         <div className='p-7 flex flex-wrap gap-4'>
           {!loading && listings.length === 0 && (
-            <p className='text-xl text-slate-700'>No Listing found!</p>
+            <p className='text-xl text-slate-700'>No listing found!</p>
           )}
           {loading && (
-            <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+            <p className='text-xl text-slate-700 text-center w-full'>
+              Loading...
+            </p>
           )}
-          {
-            !loading && listings && listings.map((listing) => (
-              <ListingItem key  = {listing.id} listing = {listing}/>
-            ))
-          }
+
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
         </div>
       </div>
     </div>
